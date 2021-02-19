ad8bf9e01841864fcd502618b0b57bc5e1443f0a
Devon Hanses <dhansen@lcs.on.ca>
2021-02-19 11:52:03 -0500

    Initial Commit

---
+++ ForismaticQuotesApp/ForismaticQuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForismaticQuotesAppApp.swift
+//  ForismaticQuotesApp
+//
+//  Created by Devon Hansen on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ForismaticQuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
